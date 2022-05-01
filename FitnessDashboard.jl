fe330a3d9bd8d2b7c9339eb9c7ca6fb566eb38b4
Jacob Zelko <jacobszelko@gmail.com>
2022-05-01 00:04:11 -0400

    Fixed plotting and label issues

--- FitnessDashboard.jl
+++ FitnessDashboard.jl
@@ -18,8 +18,12 @@ running = table_info[:running]
 weights_data =
     From(weights) |> FunSQL.render |> sql -> DBInterface.execute(db, sql) |> DataFrame
 
+weights_data.WORKOUT_DATE = Dates.Date.(weights_data.WORKOUT_DATE)
+
 running_data =
     From(running) |> FunSQL.render |> sql -> DBInterface.execute(db, sql) |> DataFrame
+
+running_data.WORKOUT_DATE = Dates.Date.(running_data.WORKOUT_DATE)
 
 pd(; x, y, name) =
     PlotData(x = x, y = y, plot = StipplePlotly.Charts.PLOT_TYPE_LINE, name = name)
@@ -29,33 +33,34 @@ export Model
 @reactive mutable struct Model <: ReactiveModel
     plot_weight_data::R{Vector{PlotData}} = [
         pd(
-            x = [Dates.Date(row.WORKOUT_DATE)],
-            y = [row.WEIGHT * row.REPS],
-            name = row.EXERCISE,
-        ) for row in eachrow(weights_data)
+            x = group.WORKOUT_DATE,
+            y = group.WEIGHT .* group.REPS,
+            name = first(group.EXERCISE),
+        ) for group in groupby(weights_data, :EXERCISE)
     ]
 
     layout_weight_data::R{PlotLayout} = PlotLayout(
-        plot_bgcolor = "#333",
         title = PlotLayoutTitle(text = "Weight Lifted Over Year", font = Font(24)),
-        xaxis = [PlotLayoutAxis(title_text = "Date")],
-        yaxis = [PlotLayoutAxis(title_text = "Total Weight Lifted per Exercise")],
+        xaxis = [PlotLayoutAxis(xy = "x", title_text = "Date")],
+        yaxis = [PlotLayoutAxis(xy = "y", title_text = "Total Weight Lifted per Exercise")],
+	showlegend = true,
     )
 
     config_weight_data::R{PlotConfig} = PlotConfig()
     
     plot_running_data::R{Vector{PlotData}} = [
         pd(
-            x = [Dates.Date(row.WORKOUT_DATE)],
-            y = [row.DISTANCE],
-            name = row.RUN_TYPE,
-        ) for row in eachrow(running_data)
+            x = group.WORKOUT_DATE,
+            y = group.DISTANCE,
+            name = first(group.RUN_TYPE),
+        ) for group in groupby(running_data, :RUN_TYPE)
     ]
 
     layout_running_data::R{PlotLayout} = PlotLayout(
-        plot_bgcolor = "#333",
         title = PlotLayoutTitle(text = "Distance Ran Over Year", font = Font(24)),
-        xaxis = [PlotLayoutAxis(title_text = "Date")],
+        xaxis = [PlotLayoutAxis(xy = "x", title_text = "Date")],
+        yaxis = [PlotLayoutAxis(xy = "y", title_text = "Miles Ran")],
+	showlegend = true,
     )
 
     config_running_data::R{PlotConfig} = PlotConfig()
